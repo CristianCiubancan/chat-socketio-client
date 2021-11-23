617ae116cf2a115d4c06de27835653ce7aa862fe
Cristian Ciubancan <balooprogames@gmail.com>
2021-11-24 00:42:22 +0200

    fixed date mismatch on queries

--- src/utils/getMessageDate.ts
+++ src/utils/getMessageDate.ts
@@ -1,9 +1,5 @@
 const getMessageDate = (messageDate: any) => {
-  const date = new Date(messageDate);
-
-  console.log("messageDate", messageDate);
-  console.log("date", date);
-  console.log("date.toString()", date.toString());
+  const date = new Date(parseInt(messageDate) * 1000);
   const day = parseInt(date.toISOString().split("-")[2]);
   const month = date.toLocaleString("default", { month: "short" });
   const dateNow = new Date();
